f85632c35d03cc84fe14fd1d53bb6eb76b07bb42
Rodrigo Barreto <rodrigofb06@gmail.com>
2024-11-13 18:13:57 -0300

    url update

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,12 @@
 const getBaseUrl = () => {
+	return "https://book-store-backend-bay-delta.vercel.app/";
+};
+
+export default getBaseUrl;
+
+/* const getBaseUrl = () => {
 	return "http://localhost:3000";
 };
 
 export default getBaseUrl;
+ */
